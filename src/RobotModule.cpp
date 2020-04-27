f87ae9f547b73bd1a377c26e7d3cf78dcf6d7cf2
Anastasia Bolotnikova <bolotnikova.anastasia@gmail.com>
2020-04-27 19:59:13 +0200

    genMemoryKeys function fix

--- src/RobotModule.cpp
+++ src/RobotModule.cpp
@@ -5,7 +5,7 @@ namespace mc_naoqi_dcm
 
 // Function to create various memory key groups
 void RobotModule::genMemoryKeys(std::string prefix, std::vector<std::string> &devices, std::string postfix,
-                                      std::vector<std::string> &memory_keys, bool isSensor=false, std::string sensor_prefix = ""){
+                                      std::vector<std::string> &memory_keys, bool isSensor, std::string sensor_prefix){
   // Create memory keys for all devices
   for (unsigned i = 0; i < devices.size(); i++){
     memory_keys.push_back("Device/SubDeviceList/" + prefix + devices[i] + postfix);
