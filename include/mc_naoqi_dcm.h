fd695c07c4843cf7753a67da1887536b2168803f
Anastasia Bolotnikova <bolotnikova.anastasia@gmail.com>
2020-08-12 18:11:30 +0200

    Removed some old comments

--- include/mc_naoqi_dcm.h
+++ include/mc_naoqi_dcm.h
@@ -125,7 +125,6 @@ class MCNAOqiDCM : public AL::ALModule
                                  AL::ALValue& ledCommands,
                                  std::string updateType="ClearAll");
   // Create aliases for all leg groups defined in robot module
-  // TODO: probably rename this function
   void createLedAliases();
 
   /**
@@ -151,8 +150,7 @@ class MCNAOqiDCM : public AL::ALModule
   // one led set function for all groups
   void setLeds(std::string ledGroupName, const float &r, const float &g, const float &b);
   void setLedsDelay(std::string ledGroupName, const float &r, const float &g, const float &b, const int& delay);
-  // TODO: better implementation/naming for similar-functionality methods
-  // cannot bind mathods with the same name and different arguments
+  // Note: cannot bind mathods with the same name and different arguments
   void isetLeds(std::string ledGroupName, const float &intensity);
   // blink
   void blink();
